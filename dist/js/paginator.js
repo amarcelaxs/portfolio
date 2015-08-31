99d6cf1785f0ee30ee321d7b8fba59466cb00eea
amarcela <ana.marcela@umbarato.com.br>
2015-08-31 01:24:44 -0300

    Layout semi pronto faltando detalhes 02

---
+++ dist/js/paginator.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+        anchors: ['home', 'about', 'education','experience', 'skill', 'contato'],
+        menu: '#menu',
+        continuousVertical: true
+    });
+});
